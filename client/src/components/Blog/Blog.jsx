30d00ecff40eae508f8487c21ffc1d11e21ccae8
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-02-01 13:50:59 +0530

    added preloader

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -5,6 +5,7 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment, FaShare } from "react-icons/fa";
 import { MdContentCopy } from "react-icons/md";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { BallTriangle } from "react-loader-spinner";
 import axios from "axios";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
@@ -22,6 +23,7 @@ export default function Blog() {
   const [copied, setCopied] = useState(false);
   const [show, setShow] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleModalClose = () => setShowModal(false);
   const handleModalShow = () => setShowModal(true);
@@ -59,6 +61,9 @@ export default function Blog() {
       .catch((err) => {
         console.log(err);
       });
+    setInterval(() => {
+      setLoading(false);
+    }, 1000);
   }, [id, loggedIn, userId]);
 
   const handleLike = (id) => {
@@ -126,119 +131,133 @@ export default function Blog() {
     <>
       <NavBar />
       <div className="blog-container">
-        <div className="utilities">
-          {loggedIn ? (
-            <>
-              <div
-                className={`utility-item like ${liked ? "liked" : ""}`}
-                onClick={() => {
-                  handleLike(id);
-                }}
-              >
-                <AiOutlineHeart />{" "}
-                <span>{blog.likes ? blog.likes.length : 0}</span>
-              </div>
-              <div className="utility-item comment" onClick={handleShow}>
-                <FaRegComment />{" "}
-                <span>{blog.comments ? blog.comments.length : 0}</span>
-              </div>
-              <div className="utility-item share" onClick={handleModalShow}>
-                <FaShare />
-              </div>
-            </>
-          ) : (
-            <div className="account-alert">Login to like and comment</div>
-          )}
-        </div>
-        <div className="blog">
-          <Card>
-            {blog.cloudinaryId ? <Card.Img src={blog.image} /> : null}
-            <Card.Body>
-              <div>
-                {blog.author}
-                <br />
-                {new Date(blog.created_at).toDateString()}
-              </div>
-              <h1>{blog.title}</h1>
-              <div
-                className="blog-content"
-                dangerouslySetInnerHTML={{ __html: blog.content }}
-              ></div>
-            </Card.Body>
-          </Card>
-        </div>
-        <Offcanvas show={show} placement="end" onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Discussion</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <div className="comment-box">
-              <Form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  addComment(id);
-                }}
-              >
-                <Form.Group>
-                  <Form.Label>Add New Comment</Form.Label>
-                  <Form.Control id="comment" as="textarea" rows="3" />
-                </Form.Group>
-                <Button variant="success" type="submit" className="mt-3">
-                  Comment
-                </Button>
-              </Form>
-            </div>
-            <div className="comment-container">
-              {blog.comments ? (
-                blog.comments.reverse().map((comment) => {
-                  return (
-                    <Card className="comment" key={comment._id}>
-                      <div className="comment-author">{comment.name}</div>
-                      <div className="comment-date">
-                        {new Date(comment.date).toDateString()}
-                      </div>
-                      <div className="comment-content">{comment.comment}</div>
-                    </Card>
-                  );
-                })
+        {loading ? (
+          <div className="loader">
+            <BallTriangle
+              radius="4px"
+              color="#8b39bb"
+              ariaLabel="loading-indicator"
+            />
+          </div>
+        ) : (
+          <>
+            <div className="utilities">
+              {loggedIn ? (
+                <>
+                  <div
+                    className={`utility-item like ${liked ? "liked" : ""}`}
+                    onClick={() => {
+                      handleLike(id);
+                    }}
+                  >
+                    <AiOutlineHeart />{" "}
+                    <span>{blog.likes ? blog.likes.length : 0}</span>
+                  </div>
+                  <div className="utility-item comment" onClick={handleShow}>
+                    <FaRegComment />{" "}
+                    <span>{blog.comments ? blog.comments.length : 0}</span>
+                  </div>
+                  <div className="utility-item share" onClick={handleModalShow}>
+                    <FaShare />
+                  </div>
+                </>
               ) : (
-                <div>No comments yet</div>
+                <div className="account-alert">Login to like and comment</div>
               )}
             </div>
-          </Offcanvas.Body>
-        </Offcanvas>
-        <Modal
-          className="copy-link"
-          show={showModal}
-          onHide={handleModalClose}
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Copy Link</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="copy-link-container">
-              <span className="url">{location}</span>{" "}
-              <CopyToClipboard
-                text={location}
-                className="copy"
-                onCopy={() => {
-                  setCopied(true);
-                  setTimeout(() => {
-                    setCopied(false);
-                  }, 1000);
-                }}
-              >
-                <MdContentCopy />
-              </CopyToClipboard>
+            <div className="blog">
+              <Card>
+                {blog.cloudinaryId ? <Card.Img src={blog.image} /> : null}
+                <Card.Body>
+                  <div>
+                    {blog.author}
+                    <br />
+                    {new Date(blog.created_at).toDateString()}
+                  </div>
+                  <h1>{blog.title}</h1>
+                  <div
+                    className="blog-content"
+                    dangerouslySetInnerHTML={{ __html: blog.content }}
+                  ></div>
+                </Card.Body>
+              </Card>
             </div>
-            {copied ? (
-              <div className="copied-text">
-                <span>Copied to clipboard</span>
-              </div>
-            ) : null}
-          </Modal.Body>
-        </Modal>
+            <Offcanvas show={show} placement="end" onHide={handleClose}>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Discussion</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div className="comment-box">
+                  <Form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      addComment(id);
+                    }}
+                  >
+                    <Form.Group>
+                      <Form.Label>Add New Comment</Form.Label>
+                      <Form.Control id="comment" as="textarea" rows="3" />
+                    </Form.Group>
+                    <Button variant="success" type="submit" className="mt-3">
+                      Comment
+                    </Button>
+                  </Form>
+                </div>
+                <div className="comment-container">
+                  {blog.comments ? (
+                    blog.comments.reverse().map((comment) => {
+                      return (
+                        <Card className="comment" key={comment._id}>
+                          <div className="comment-author">{comment.name}</div>
+                          <div className="comment-date">
+                            {new Date(comment.date).toDateString()}
+                          </div>
+                          <div className="comment-content">
+                            {comment.comment}
+                          </div>
+                        </Card>
+                      );
+                    })
+                  ) : (
+                    <div>No comments yet</div>
+                  )}
+                </div>
+              </Offcanvas.Body>
+            </Offcanvas>
+            <Modal
+              className="copy-link"
+              show={showModal}
+              onHide={handleModalClose}
+              centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Copy Link</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="copy-link-container">
+                  <span className="url">{location}</span>{" "}
+                  <CopyToClipboard
+                    text={location}
+                    className="copy"
+                    onCopy={() => {
+                      setCopied(true);
+                      setTimeout(() => {
+                        setCopied(false);
+                      }, 1000);
+                    }}
+                  >
+                    <MdContentCopy />
+                  </CopyToClipboard>
+                </div>
+                {copied ? (
+                  <div className="copied-text">
+                    <span>Copied to clipboard</span>
+                  </div>
+                ) : null}
+              </Modal.Body>
+            </Modal>
+          </>
+        )}
       </div>
       <Footer />
     </>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Container, Card } from "react-bootstrap";
 import { AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
+import { BallTriangle } from "react-loader-spinner";
 import axios from "axios";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
@@ -11,6 +12,7 @@ import "./Home.css";
 export default function Home() {
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
@@ -22,6 +24,9 @@ export default function Home() {
       .catch((err) => {
         console.log(err);
       });
+    setInterval(() => {
+      setLoading(false);
+    }, 1000);
   }, []);
 
   const handlePost = (id) => {
@@ -33,45 +38,55 @@ export default function Home() {
       <NavBar />
       <div className="home-container">
         <h1 className="main-heading">Recent Blogs</h1>
-        <Container>
-          {blogs.length > 0 ? (
-            blogs.reverse().map((blog) => {
-              return (
-                <Card
-                  className="blog-card"
-                  key={blog._id}
-                  onClick={() => {
-                    handlePost(blog._id);
-                  }}
-                >
-                  {blog.cloudinaryId ? (
-                    <Card.Img variant="top" src={blog.image} />
-                  ) : null}
-                  <Card.Body>
-                    <h1>{blog.title}</h1>
-                    <div className="blog-info">{blog.author}</div>
-                    <div className="blog-info">
-                      {new Date(blog.created_at).toDateString()}
-                    </div>
-                    <div className="blog-items">
-                      <span>
-                        <AiOutlineHeart /> {blog.likes.length} Reactions
-                      </span>
-                      <span>
-                        <FaRegComment /> {blog.comments.length} Comments
-                      </span>
-                    </div>
-                  </Card.Body>
-                </Card>
-              );
-            })
-          ) : (
-            <>
-              <h1>No Blogs Avaliable</h1>
-              <p>Write Your own Blog !!</p>
-            </>
-          )}
-        </Container>
+        {loading ? (
+          <div className="loader">
+            <BallTriangle
+              radius="4px"
+              color="#8b39bb"
+              ariaLabel="loading-indicator"
+            />
+          </div>
+        ) : (
+          <Container>
+            {blogs.length > 0 ? (
+              blogs.reverse().map((blog) => {
+                return (
+                  <Card
+                    className="blog-card"
+                    key={blog._id}
+                    onClick={() => {
+                      handlePost(blog._id);
+                    }}
+                  >
+                    {blog.cloudinaryId ? (
+                      <Card.Img variant="top" src={blog.image} />
+                    ) : null}
+                    <Card.Body>
+                      <h1>{blog.title}</h1>
+                      <div className="blog-info">{blog.author}</div>
+                      <div className="blog-info">
+                        {new Date(blog.created_at).toDateString()}
+                      </div>
+                      <div className="blog-items">
+                        <span>
+                          <AiOutlineHeart /> {blog.likes.length} Reactions
+                        </span>
+                        <span>
+                          <FaRegComment /> {blog.comments.length} Comments
+                        </span>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                );
+              })
+            ) : (
+              <>
+                <h1>No Blogs Avaliable</h1>
+                <p>Write Your own Blog !!</p>
+              </>
+            )}
+          </Container>
+        )}
       </div>
       <Footer />
     </>
