6d5029ef87b00db6851846fb25d5cbe8d59d34c0
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-02-01 14:02:36 +0530

    added preloader in profile component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { Card, ListGroup, Button } from "react-bootstrap";
 import Identicon from "react-identicons";
 import { AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
+import { BallTriangle } from "react-loader-spinner";
 import axios from "axios";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
@@ -17,6 +18,7 @@ export default function Profile() {
   const [blogs, setBlogs] = useState([]);
   const [likedBlogs, setLikedBlogs] = useState([]);
   const [isBlog, setIsBlog] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
@@ -36,6 +38,9 @@ export default function Profile() {
     } else {
       navigate("/login");
     }
+    setInterval(() => {
+      setLoading(false);
+    }, 1000);
   }, [navigate, setName, setEmail, setBlogs, setLikedBlogs]);
 
   const handlePost = (id) => {
@@ -102,31 +107,97 @@ export default function Profile() {
             </Card.Body>
           </Card>
         </div>
-        <div className="profile-blogs">
-          {isBlog ? (
-            <>
-              <h1 className="main-heading">Your Blogs</h1>
-              <div>
-                {blogs.map((blog) => {
-                  return (
-                    <Card className="blog-card" key={blog._id}>
-                      {blog.cloudinaryId ? (
-                        <Card.Img variant="top" src={blog.image} />
-                      ) : null}
-                      <Card.Body>
-                        <h1
-                          onClick={() => {
-                            handlePost(blog._id);
-                          }}
-                        >
-                          {blog.title}
-                        </h1>
-                        <div className="blog-info">{blog.author}</div>
-                        <div className="blog-info">
-                          {new Date(blog.created_at).toDateString()}
-                        </div>
-                        <div className="blog-items">
-                          <div>
+        {loading ? (
+          <div className="loader">
+            <BallTriangle
+              radius="4px"
+              color="#8b39bb"
+              ariaLabel="loading-indicator"
+            />
+          </div>
+        ) : (
+          <div className="profile-blogs">
+            {isBlog ? (
+              <>
+                <h1 className="main-heading">Your Blogs</h1>
+                <div>
+                  {blogs.map((blog) => {
+                    return (
+                      <Card className="blog-card" key={blog._id}>
+                        {blog.cloudinaryId ? (
+                          <Card.Img variant="top" src={blog.image} />
+                        ) : null}
+                        <Card.Body>
+                          <h1
+                            onClick={() => {
+                              handlePost(blog._id);
+                            }}
+                          >
+                            {blog.title}
+                          </h1>
+                          <div className="blog-info">{blog.author}</div>
+                          <div className="blog-info">
+                            {new Date(blog.created_at).toDateString()}
+                          </div>
+                          <div className="blog-items">
+                            <div>
+                              <span>
+                                <AiOutlineHeart /> {blog.likes.length} Reactions
+                              </span>
+                              <span>
+                                <FaRegComment /> {blog.comments.length} Comments
+                              </span>
+                            </div>
+                            <div>
+                              <Button
+                                className="edit-button"
+                                variant="info"
+                                onClick={() => {
+                                  handleEdit(blog._id);
+                                }}
+                              >
+                                Edit
+                              </Button>
+                              <Button
+                                className="edit-button"
+                                variant="danger"
+                                onClick={() => {
+                                  handleDelete(blog._id);
+                                }}
+                              >
+                                Delete
+                              </Button>
+                            </div>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    );
+                  })}
+                </div>
+              </>
+            ) : (
+              <>
+                <h1 className="main-heading">Liked Blogs</h1>
+                <div>
+                  {likedBlogs.map((blog) => {
+                    return (
+                      <Card
+                        className="blog-card"
+                        key={blog._id}
+                        onClick={() => {
+                          handlePost(blog._id);
+                        }}
+                      >
+                        {blog.cloudinaryId ? (
+                          <Card.Img variant="top" src={blog.image} />
+                        ) : null}
+                        <Card.Body>
+                          <h1>{blog.title}</h1>
+                          <div className="blog-info">{blog.author}</div>
+                          <div className="blog-info">
+                            {new Date(blog.created_at).toDateString()}
+                          </div>
+                          <div className="blog-items">
                             <span>
                               <AiOutlineHeart /> {blog.likes.length} Reactions
                             </span>
@@ -134,71 +205,15 @@ export default function Profile() {
                               <FaRegComment /> {blog.comments.length} Comments
                             </span>
                           </div>
-                          <div>
-                            <Button
-                              className="edit-button"
-                              variant="info"
-                              onClick={() => {
-                                handleEdit(blog._id);
-                              }}
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              className="edit-button"
-                              variant="danger"
-                              onClick={() => {
-                                handleDelete(blog._id);
-                              }}
-                            >
-                              Delete
-                            </Button>
-                          </div>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  );
-                })}
-              </div>
-            </>
-          ) : (
-            <>
-              <h1 className="main-heading">Liked Blogs</h1>
-              <div>
-                {likedBlogs.map((blog) => {
-                  return (
-                    <Card
-                      className="blog-card"
-                      key={blog._id}
-                      onClick={() => {
-                        handlePost(blog._id);
-                      }}
-                    >
-                      {blog.cloudinaryId ? (
-                        <Card.Img variant="top" src={blog.image} />
-                      ) : null}
-                      <Card.Body>
-                        <h1>{blog.title}</h1>
-                        <div className="blog-info">{blog.author}</div>
-                        <div className="blog-info">
-                          {new Date(blog.created_at).toDateString()}
-                        </div>
-                        <div className="blog-items">
-                          <span>
-                            <AiOutlineHeart /> {blog.likes.length} Reactions
-                          </span>
-                          <span>
-                            <FaRegComment /> {blog.comments.length} Comments
-                          </span>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  );
-                })}
-              </div>
-            </>
-          )}
-        </div>
+                        </Card.Body>
+                      </Card>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+          </div>
+        )}
       </div>
       <Footer />
     </>
